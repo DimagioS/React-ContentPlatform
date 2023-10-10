d4c94e399076e8027ce33d1a6a137890e879347b
Dmitry Sushkov <sushkovd98@mail.ru>
2023-10-10 23:01:10 +0200

    Integrate React and Style Loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,13 +2,24 @@ import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
 
+    // this loader can process jsx, otherwise babel would be needed
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
     }
 
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      }
+
     return [
         typescriptLoader,
+        cssLoader,
     ]
 }
