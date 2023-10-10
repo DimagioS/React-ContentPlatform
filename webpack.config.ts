1acab486eb3f46b60812778f286a9732af7f69ff
Dmitry Sushkov <sushkovd98@mail.ru>
2023-10-10 18:42:49 +0200

    Implement Webpack Dev Server

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,21 +1,26 @@
 import path from "path";
 import webpack from 'webpack';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildMode, BuildPaths } from './config/build/types/config';
+import { BuildEnv, BuildMode, BuildPaths } from './config/build/types/config';
 
-const paths: BuildPaths = {
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    build: path.resolve(__dirname, 'build'),
-    html: path.resolve(__dirname, 'public', 'index.html'),
-}
+export default (env: BuildEnv) => {
+    const paths: BuildPaths = {
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+    }
+    
+    const mode: BuildMode = env.mode || 'development';
+    const port = env.port || 3000;
 
-const mode: BuildMode = 'development';
-const isDev: boolean = mode === 'development';
+    const isDev: boolean = mode === 'development';
 
-const config: webpack.Configuration = buildWebpackConfig({
-    mode,
-    paths,
-    isDev,
-})
+    const config: webpack.Configuration = buildWebpackConfig({
+        mode,
+        paths,
+        isDev,
+        port
+    }) 
 
-export default config;
+    return config
+};
