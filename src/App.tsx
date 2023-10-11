dac074e23c316137dda6c5c1c4ab47a8e8b61f81
Dmitry Sushkov <sushkovd98@mail.ru>
2023-10-11 17:25:22 +0200

    Add classNames utility function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { Route, Routes, Link } from "react-router-dom";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 import MainPage from "./pages/MainPage";
 import AboutPage from "./pages/AboutPage";
 import './styles/index.scss'
@@ -9,7 +10,7 @@ const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
