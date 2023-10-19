c955cfa8a3fb44710776c17e1c54993d2e725015
Dmitry Sushkov <sushkovd98@mail.ru>
2023-10-19 17:57:34 +0200

    Add: Implement Error Boundaries

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,24 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import styles from './PageError.module.scss';
+
+interface pageErrorProps {
+  className?: string;
+}
+
+export const PageError = ({ className }: pageErrorProps) => {
+  const { t } = useTranslation();
+
+  function reloadPage() {
+    // eslint-disable-next-line no-restricted-globals
+    location.reload();
+  }
+
+  return (
+    <div className={classNames(styles.PageError, {}, [className])}>
+      <Button onClick={reloadPage} type="button">{t('Обновить страницу')}</Button>
+      <h1>{t('Что-то пошло не так')}</h1>
+    </div>
+  );
+};
